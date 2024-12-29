102640a9df422104696cf200f7ccfa2718311a44
Glitch (netsuite-slack-app) <none>
2024-12-29 16:37:57 +0000

    ✅🪒 Updated with Glitch

---
+++ app.js
@@ -0,0 +1,19 @@
+// Require the Bolt package (github.com/slackapi/bolt)
+const { App } = require("@slack/bolt");
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN 
+});
+
+
+//Add code here
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
